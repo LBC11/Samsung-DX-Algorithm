72d958d40a5e664dcd0abdeca35c90099a905735
LBC <wpdltm4@gmail.com>
2023-01-27 00:01:54 +0900

    모의테스트

---
+++ test1_Solution.java
@@ -0,0 +1,137 @@
+package Samsung;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+문제 요약
+    A~Z 사이의 대문자로 이루어진 2-D array 가 주어진다.
+    각 index 는 여행지를 그리고 대문자는 여행지의 기념품을 의미한다.
+    한정된 예산으로 인해 같은 기념품을 2개 이상 구매할 수 없다.
+    이동은 가로 혹은 세로로 1칸 움직인다.
+    (0,0) 에서 출발할 때 가장 많은 기념품을 구매할 수 있는 경로의
+    기념품 수를 구하시오.
+
+1. 실패 이유 분석
+
+    heap 영역에 저장되는 ArrayList, boolean[]를 사용했어서
+    각각 route 마다 독립적으로 저장해야할 purchased 정보를
+    모든 route 가 공유하는 것이 아닐까?
+
+    단순히 할당하는 것은 참조값만 복제하는 것이다.
+    객체 자체를 복제하고자 clone()이용
+
+    여행지가 "A" 같이 하나만 있을 경우 code 에서 범위 제한에
+    걸리게 되어 기존의 max = 0 을 갱신하지 못했다.
+    이를 방지하기 위해 max = 1 을 기본값으로 변경
+
+    더 이상 움직이지 못하는 경우 max 값을 갱신할 수 없어
+    범위 검증 if 문에 max 값 갱신을 위한 else 문 추가
+
+2. 개선점
+
+    굳이 ArrayList<Character> 를 사용할 이유가 있을까?
+    char 에서 대문자의 범위가 65~90인 것을 이용해서
+    boolean[] 으로 대체함.
+    access 는 "char value - 65" 로 대체하였다.
+
+*/
+
+public class test1_Solution {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuffer sb = new StringBuffer();
+
+    static char[][] places;
+    static int[] x_move = {0,0,1,-1};
+    static int[] y_move = {1,-1,0,0};
+    static int max = 1;
+
+    static void visit(int x, int y, boolean[] purchased) {
+
+        // 각각의 route 가 같은 purchased 정보 공유 방지
+        boolean[] p = purchased.clone();
+
+        // 방문한 도시의 기념품이 이미 구매한 기념품일 경우
+        if(p[places[x][y] - 65]) {
+
+            // 구매한 상품 개수 세기
+            int num = 0;
+            for (boolean b : p) {
+                if (b) num++;
+            }
+
+            // max 값 갱신
+            if(num > max) max = num;
+        }
+
+        // 방문한 도시의 기념품이 구매한 상품이 아닐 경우
+        else {
+
+            // 기념품 구매
+            p[places[x][y] - 65] = true;
+
+            for(int i=0; i<4; i++) {
+
+                // 1칸 이동
+                int x_moved = x+x_move[i];
+                int y_moved = y+y_move[i];
+
+                // 이동한 index 가 배열의 범위 내에 있을 때
+                if(0 <= x_moved && x_moved < places.length && 0 <= y_moved && y_moved < places[0].length) {
+
+                    // 새로운 여행지 방문
+                    visit(x_moved, y_moved, p);
+                }
+
+                // max 값 갱신
+                else {
+                    // 구매한 상품 개수 세기
+                    int num = 0;
+                    for (boolean b : p) {
+                        if (b) num++;
+                    }
+
+                    // max 값 갱신
+                    if(num > max) max = num;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // t번 반복
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            places = new char[r][c];
+            for(int j=0; j<r; j++) {
+                String temp = br.readLine();
+                for(int k=0; k<c; k++) {
+                    places[j][k] = temp.charAt(k);
+                }
+            }
+
+            // 여행 시작
+            visit(0,0, new boolean[26]);
+
+            // 결과 입력
+            sb.append("#").append(i+1).append(" ").append(max).append("\n");
+
+            // max value 초기화
+            max = 0;
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
---
+++ test2_Solution.java
@@ -0,0 +1,154 @@
+package Samsung;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+문제 요약
+    주어진 N보다 작으면서 X, Y 두개의 숫자로 이루어진 수 중 가장 큰 수를 구하시오.
+    1 <= N <= 10^100000, 0<= X < Y <=9
+
+1. 실패이유 분석
+    N의 범위가 1~10^100000 이므로 for 문으로 일일이 뒤에 오는 y를
+    더하는 것에 너무 많은 시간을 소모한다. 문자열을 빠르게 repeat
+    할 수 있는 방법이 필요하다.
+
+    repeat() 함수는 java 11 version 부터 가능해서 불가능
+    new String(new char[n]).replace("\0", "반복할 단어") 사용
+
+    case: 1200 0 4 에서 반례가 만들어짐. Generator 에서
+    result 를 return 하기 전에 index 0의 value 가 0이면 삭제
+
+    case: 1202 3 9 에서 반례가 만들어짐. result 의 자릿수를 N보다
+    1 작게 해야하는데 뺴놓음.
+
+ */
+
+public class test2_Solution {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuffer sb = new StringBuffer();
+
+    static String Generator(String N, int x, int y) {
+
+        // 선물할 수가 없는 경우
+        if (N.length() == 1) {
+            if(x > Integer.parseInt(N) || (x == 0 && y > Integer.parseInt(N))) {
+                return "-1";
+            }
+        }
+
+        int length = N.length();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int num = N.charAt(i) - '0';
+
+            if (num > y) {
+
+                // 남은 모든 자릿수가 y이다.
+                result.append(new String(new char[length - i]).replace("\0", String.valueOf(y)));
+
+                break;
+            }
+
+            else if (num == y) {
+
+                // 현재 위치의 자릿수가 y이다.
+                result.append(y);
+            }
+
+            // num 이 x와 y 사이의 수일 경우
+            else if (num > x) {
+
+                // 현재 위치의 자릿수가 x이고
+                result.append(x);
+
+                // 남은 모든 자릿수가 모두 y이다.
+                result.append(new String(new char[length - i - 1]).replace("\0", String.valueOf(y)));
+
+                break;
+            }
+
+            else if (num == x) {
+
+                // 현재 위치의 자릿수가 x이다.
+                result.append(x);
+            }
+
+            // x가 num 보다 클 경우
+            else {
+
+                // 지금까지 완성한 result 의 index
+                int idx = i;
+
+                // idx 가 0이상이어야 한다.
+                while(true) {
+
+                    // 지금의 위치가 제일 큰 자리일 경우
+                    if (idx == 0) {
+
+                        // result 의 길이가 N의 길이보다 1 작아짐
+                        // 모든 자릿수가 y이다.
+                        result.replace(0, result.length(), new String(new char[length - 1]).replace("\0", String.valueOf(y)));
+
+                        break;
+                    }
+
+                    // 그렇지 않을 경우 이전 idx 의 value 확인
+                    else idx--;
+
+                    // 이전 idx 의 value 가 y일 경우
+                    if (result.charAt(idx) - '0' == y) {
+
+                        // 기존의 y와 그 뒤의 자릿수를 "x"로 교체
+                        result.replace(idx, result.length(), x + new String(new char[length - idx - 1]).replace("\0", String.valueOf(y)));
+
+                        break;
+                    }
+
+                    // 이전 idx 의 value 가 x일 경우
+                    if (result.charAt(idx) - '0' == x) {
+
+                        // 반복문 재 실행
+                        continue;
+                    }
+                }
+
+                //
+                break;
+            }
+        }
+
+        if(result.charAt(0) == '0') result.deleteCharAt(0);
+
+        return result.toString();
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        // t번 반복
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            // N은 Int 나 Long 의 범위를 넘을 수 있기에 String 으로 받는다.
+            String N = st.nextToken();
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            sb.append("#").append(i + 1).append(" ").append(Generator(N, x, y)).append("\n");
+
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
