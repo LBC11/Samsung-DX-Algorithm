46643cf7445474987e9b7cd2c820dcedabb1dc09
LBC <wpdltm4@gmail.com>
2023-02-01 00:48:20 +0900

    Add the input, output data

---
+++ 연결리스트_2/No_5/No_5.java
@@ -0,0 +1,22 @@
+package 연결리스트_2.No_5;
+
+import java.io.*;
+/*
+수열 편집
+
+ */
+public class No_5 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
--- 연결리스트_2/No_7/Solution.java
+++ 연결리스트_2/No_7/Solution.java
@@ -1,71 +1,90 @@
-package 연결리스트.No_5;
+package 연결리스트_2.No_7;
 
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+Doubly Linked List 구현하기
+ */
 public class Solution {
 
     private final static int MAX_NODE = 10000;
     private final static int ADD_HEAD = 1;
     private final static int ADD_TAIL = 2;
     private final static int ADD_NUM = 3;
-    private final static int REMOVE = 4;
-    private final static int PRINT = 5;
+    private final static int FIND = 4;
+    private final static int REMOVE = 5;
+    private final static int PRINT = 6;
+    private final static int PRINT_REVERSE = 7;
     private final static int END = 99;
 
     private final static UserSolution usersolution = new UserSolution();
 
     private static BufferedReader br;
 
-    private static void run() throws Exception {
-
+    public static void run() throws Exception {
         int cmd, data, num, ret;
         int[] output = new int[MAX_NODE];
         String str;
         StringTokenizer st;
 
-        while(true) {
+        while (true) {
             str = br.readLine();
             st = new StringTokenizer(str, " ");
             cmd = Integer.parseInt(st.nextToken());
 
-            switch(cmd) {
-                case ADD_HEAD :
+            switch (cmd) {
+                case ADD_HEAD:
                     data = Integer.parseInt(st.nextToken());
                     usersolution.addNode2Head(data);
                     break;
 
-                case ADD_TAIL :
+                case ADD_TAIL:
                     data = Integer.parseInt(st.nextToken());
                     usersolution.addNode2Tail(data);
                     break;
 
-                case ADD_NUM :
+                case ADD_NUM:
                     data = Integer.parseInt(st.nextToken());
                     num = Integer.parseInt(st.nextToken());
                     usersolution.addNode2Num(data, num);
                     break;
 
-                case REMOVE :
+                case FIND:
+                    data = Integer.parseInt(st.nextToken());
+                    num = usersolution.findNode(data);
+                    System.out.println(num);
+                    break;
+
+                case REMOVE:
                     data = Integer.parseInt(st.nextToken());
                     usersolution.removeNode(data);
                     break;
 
-                case PRINT :
+                case PRINT:
                     ret = usersolution.getList(output);
-                    for(int i = 0; i < ret; i++) {
+                    for (int i = 0; i < ret; i++) {
                         System.out.print(output[i] + " ");
                     }
                     System.out.println();
                     break;
 
-                case END :
+                case PRINT_REVERSE:
+                    ret = usersolution.getReversedList(output);
+                    for (int i = 0; i < ret; i++) {
+                        System.out.print(output[i] + " ");
+                    }
+                    System.out.println();
+                    break;
+
+                case END:
                     return;
             }
         }
     }
+
     public static void main(String[] args) throws Exception {
         int TC;
         //System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
@@ -81,5 +100,4 @@ public class Solution {
             System.out.println();
         }
     }
-
 }
