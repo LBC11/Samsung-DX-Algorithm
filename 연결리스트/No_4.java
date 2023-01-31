9b4c904f03afcd1c83fe9a09be666958f61da276
LBC <wpdltm4@gmail.com>
2023-01-31 21:24:20 +0900

    기초 Singly Linked List 연습

---
+++ 연결리스트/No_4.java
@@ -0,0 +1,23 @@
+package 연결리스트;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class No_4 {
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuffer sb = new StringBuffer();
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
