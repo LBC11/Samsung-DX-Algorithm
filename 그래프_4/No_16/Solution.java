ff57b29db573f7f5e247e062c018abd85844bcdf
LBC <wpdltm4@gmail.com>
2023-02-13 23:45:10 +0900

    temp

--- 그래프_4/No_16/Solution.java
+++ 그래프_4/No_16/Solution.java
@@ -1,5 +1,7 @@
 package 그래프_4.No_16;
 
+import org.w3c.dom.Node;
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -11,13 +13,62 @@ public class Solution {
 
     static StringTokenizer st;
 
+    static int[] x;
+    static int[] y;
+    static int[] parent;
+
     public static void main(String[] args) throws IOException {
 
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1; t<=T; t++) {
+
+            int n = Integer.parseInt(br.readLine());
+
+            x = new int[n];
+            y = new int[n];
+            parent = new int[n];
+
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<n; i++) {
+
+                // x value
+                x[i] = Integer.parseInt(st.nextToken());
+
+                // 초기 parent 값은 자기 자신
+                parent[i] = i;
+            }
+
+            st = new StringTokenizer(br.readLine());
+
+            for(int i=0; i<n; i++) {
+
+                // y value
+                y[i] = Integer.parseInt(st.nextToken());
+            }
+
+
+
+
+
+        }
 
         bw.write(sb.toString());
         bw.flush();
         br.close();
         bw.close();
 
+    }
+}
+
+class Edge {
+
+    int start;
+    int end;
+    long distance;
+
+    public Edge(int start, int end, long distance) {
+        this.start = start;
+        this.end = end;
+        this.distance = distance;
     }
 }
