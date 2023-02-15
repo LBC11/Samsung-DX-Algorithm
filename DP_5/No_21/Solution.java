92f74c0083bf0b7d90779e5f863fdb6a1af6ad6a
LBC <wpdltm4@gmail.com>
2023-02-15 23:17:19 +0900

    temp

--- DP_5/No_21/Solution.java
+++ DP_5/No_21/Solution.java
@@ -3,6 +3,9 @@ package DP_5.No_21;
 import java.io.*;
 import java.util.StringTokenizer;
 
+/*
+
+ */
 public class Solution {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,7 +14,28 @@ public class Solution {
 
     static StringTokenizer st;
 
+    static int[][] item;
+
     public static void main(String[] args) throws IOException {
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            item = new int[n][2];
+            for(int i=0; i<n; i++) {
+
+                st = new StringTokenizer(br.readLine());
+
+                item[i][0] = Integer.parseInt(st.nextToken());
+                item[i][1] = Integer.parseInt(st.nextToken());
+            }
+
+
+        }
 
 
         bw.write(sb.toString());
