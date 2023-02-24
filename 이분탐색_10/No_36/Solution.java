e412f1b3d1ad22bab93954d18937a5c80853176c
LBC <wpdltm4@gmail.com>
2023-02-24 23:20:48 +0900

    temp

--- 이분탐색_10/No_36/Solution.java
+++ 이분탐색_10/No_36/Solution.java
@@ -10,12 +10,38 @@ public class Solution {
     static StringBuffer sb = new StringBuffer();
     static StringTokenizer st;
 
+    static int solution(int[] study, int p) {
+
+        int ans = 0;
+
+        int start = 0;
+        int end = 0;
+
+        while(true) {
+
+            if()
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
 
-        int T= Integer.parseInt(br.readLine());
-        for(int t=1; t<=T; t++) {
+            st = new StringTokenizer(br.readLine());
 
-            sb.append("#").append(" ").append(t).append(" ").append("\n");
+            int n = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            int[] study = new int[n];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                study[i] = Integer.parseInt(st.nextToken());
+            }
+
+            sb.append("#").append(t).append(" ").append(solution(study, p)).append("\n");
         }
 
         bw.write(sb.toString());
