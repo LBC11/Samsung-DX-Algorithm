7ac228eabcee145acc7c332e40a6962c7ce24deb
LBC <wpdltm4@gmail.com>
2023-03-03 14:35:44 +0900

    temp

--- 해시_8/No_32/Solution.java
+++ 해시_8/No_32/Solution.java
@@ -1,26 +1,102 @@
 package 해시_8.No_32;
 
-import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class Solution {
+class Solution {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuffer sb = new StringBuffer();
-    static StringTokenizer st;
+    private static int mSeed = 5;
 
-    public static void main(String[] args) throws IOException {
+    private static final int MAXL = 50005;
+    private static final int DUMMY_LEN = 5959;
+    private static int score = 0;
+    private static int total_score = 0;
 
-        int T= Integer.parseInt(br.readLine());
-        for(int t=1; t<=T; t++) {
+    private static Scanner sc;
+    private static UserSolution user = new UserSolution();
 
-            sb.append("#").append(" ").append(t).append(" ").append("\n");
+    private static char dummy1[] = new char[DUMMY_LEN];
+    private static char init_string[] = new char[MAXL];
+    private static char dummy2[] = new char[DUMMY_LEN];
+    private static char result_string[] = new char[MAXL];
+    private static char dummy3[] = new char[DUMMY_LEN];
+    private static char user_ans_string[] = new char[MAXL];
+    private static char dummy4[] = new char[DUMMY_LEN];
+    private static char string_A[] = new char[4];
+    private static char string_B[] = new char[4];
+    private static int init_string_len = 0;
+    private static int char_type = 0;
+    private static int query_cnt = 0;
+
+    private static int pseudo_rand()
+    {
+        mSeed = mSeed * 214013 + 2531011;
+        return (mSeed >> 16) & 0x7FFF;
+    }
+
+    public static void main(String[] args) throws Exception {
+//		System.setIn(new java.io.FileInputStream("sample_input.txt"));
+
+        sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        total_score = 0;
+        for (int TC = 1; TC <= T; TC++) {
+            score = 100;
+            mSeed = sc.nextInt();
+            init_string_len = sc.nextInt();
+            char_type = sc.nextInt();
+            query_cnt = sc.nextInt();
+
+            for (int i = 0; i < init_string_len; i++)
+            {
+                init_string[i] = (char)(pseudo_rand() % char_type + 'a');
+            }
+            init_string[init_string_len] = '\0';
+
+            user.init(init_string_len, init_string);
+
+            string_A[3] = string_B[3] = '\0';
+            for (int i = 0; i < query_cnt; i++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    string_A[k] = (char) ((pseudo_rand() % char_type) + 'a');
+                    string_B[k] = (char) ((pseudo_rand() % char_type) + 'a');
+                }
+                int user_ans = user.change(string_A, string_B);
+                int ans = sc.nextInt();
+
+                System.out.println("user_ans: "+user_ans+", ans: "+ans);
+
+                if (ans != user_ans)
+                {
+                    score = 0;
+                }
+
+                System.out.println(score);
+            }
+
+            user.result(user_ans_string);
+
+            sc.nextLine();
+            String result_str_java = sc.nextLine();
+            result_string = result_str_java.toString().toCharArray();
+
+            for (int i = 0; i < init_string_len; i++)
+            {
+                System.out.println(i+"번쨰 "+"result_string: "+result_string[i] + " user_string: "+user_ans_string[i]);
+                if (result_string[i] != user_ans_string[i])
+                {
+                    score = 0;
+                    break;
+                }
+            }
+
+            total_score += score;
+            System.out.println("#" + TC +" " + score);
+
         }
-
-        bw.write(sb.toString());
-        bw.flush();
-        br.close();
-        bw.close();
+        System.out.println("Total score : " + total_score/T);
+        sc.close();
     }
 }
---
+++ 해시_8/No_32/UserSolution.java
@@ -0,0 +1,184 @@
+package 해시_8.No_32;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserSolution {
+
+    private final long Hash_size = 1 << 30;
+    private final long DIV = Hash_size - 1;
+
+    HashMap<Integer, String> record = new HashMap<>();
+
+    int[] pack_numbers;
+
+    void init(int N, char[] init_string) {
+
+        pack_numbers = new int[N - 2];
+
+        for (int i = 0; i < N - 2; i++) {
+            pack_numbers[i] = bitPacking(init_string, i);
+        }
+    }
+
+    int change(char[] string_A, char[] string_B) {
+
+        int num_A = bitPacking(string_A, 0);
+        int num_B = bitPacking(string_B, 0);
+
+        int ret = 0;
+
+        ArrayList<Integer> revise_idx = new ArrayList<>();
+
+        for (int i = 0; i < pack_numbers.length; i++) {
+
+            if (pack_numbers[i] == num_A) {
+                pack_numbers[i] = num_B;
+
+                revise_idx.add(i);
+
+                // 그 뒤 2번째 까지 문자열이 바뀌면서 기존의 packing 값도 다시 계산해야 한다.
+                i += 2;
+
+                ret++;
+            }
+        }
+
+        // change 되면서 영향 받은 hash 값들 재조정
+        for (int i : revise_idx) {
+
+            char[] unpack = unpack_all(pack_numbers[i]);
+            char[] new_string;
+
+            if (i - 2 >= 0) {
+
+                // i - 2번째 hash 값 재조정
+                new_string = unpack_all(pack_numbers[i - 2]);
+
+                new_string[2] = unpack[0];
+
+                pack_numbers[i - 2] = bitPacking(new_string, 0);
+            }
+
+            if (i - 1 >= 0) {
+
+                // i - 1번째 hash 값 재조정
+                new_string = unpack_all(pack_numbers[i - 1]);
+
+                new_string[1] = unpack[0];
+                new_string[2] = unpack[1];
+
+                pack_numbers[i - 1] = bitPacking(new_string, 0);
+            }
+
+            if (i + 1 < pack_numbers.length) {
+
+                // i + 1번째 hash 값 재조정
+                new_string = unpack_all(pack_numbers[i + 1]);
+
+                new_string[0] = unpack[1];
+                new_string[1] = unpack[2];
+
+                pack_numbers[i + 1] = bitPacking(new_string, 0);
+
+            }
+
+            if (i + 2 < pack_numbers.length) {
+
+                // i + 2번째 hash 값 재조정
+                new_string = unpack_all(pack_numbers[i + 2]);
+
+                new_string[0] = unpack[2];
+
+                pack_numbers[i + 2] = bitPacking(new_string, 0);
+            }
+        }
+
+        return ret;
+    }
+
+    void result(char[] ret) {
+
+        for (int i = 0; i < pack_numbers.length - 1; i++) {
+
+            ret[i] = unpack_first(pack_numbers[i]);
+        }
+
+        char[] rets = unpack_all(pack_numbers[pack_numbers.length - 1]);
+
+        for (int i = 0; i < 3; i++) {
+            ret[pack_numbers.length - 1 + i] = rets[i];
+        }
+    }
+
+    int getHash(char[] arr, int start) {
+
+        long hash = 5381;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < start + 3; i++) {
+
+            hash = (hash << 3) + hash + arr[i];
+            sb.append(arr[i]);
+        }
+
+        int ret = (int) (hash & DIV);
+
+        record.put(ret, sb.toString());
+
+        return ret;
+    }
+
+    int bitPacking(char[] arr, int start) {
+
+        int ans = 0;
+
+        int j = 0;
+        for (int i = start; i < start + 3; i++) {
+
+            int num = arr[i] - 'a';
+
+            // num 의 j번째 bit 값이 1이면 ans 에 해당 bit 값 더하기
+            for (int t = 0; t < 5; t++) {
+                if ((num & (1 << t)) != 0) ans += (1 << j);
+                j++;
+            }
+        }
+
+        return ans;
+    }
+
+    // 첫번째 글자만 unpack 후 return
+    char unpack_first(int packed) {
+
+        int num = 0;
+
+        for (int i = 10; i < 15; i++) {
+
+            if ((packed & (1 << i)) != 0) num += (1 << (i - 10));
+        }
+
+        return (char) num;
+    }
+
+    // 모든 글자 unpack 후 return
+    char[] unpack_all(int packed) {
+
+        char[] ret = new char[3];
+
+        for (int i = 0; i < 3; i++) {
+
+            int num = 0;
+
+            for (int j = i * 5; j < i * 5 + 5; j++) {
+                if ((packed & (1 << j)) != 0) num += (1 << (j - i * 5));
+            }
+
+            // unpack 한 글자
+            ret[i] = (char) num;
+        }
+
+        return ret;
+    }
+}
