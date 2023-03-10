893156a0007bb25054322abde50e3061a2f97880
LBC <wpdltm4@gmail.com>
2023-03-10 17:47:35 +0900

    kakao

---
+++ Kakao/delivery_2023/Solution.java
@@ -0,0 +1,11 @@
+package Kakao.delivery_2023;
+
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+
+        long answer = 0;
+
+
+        return answer;
+    }
+}
